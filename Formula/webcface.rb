e966cb1101322c74f7510a45f3bf9adf30f3b52c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-09-28 00:41:02 +0900

    fix audit error

--- Formula/webcface.rb
+++ Formula/webcface.rb
@@ -5,16 +5,14 @@ class Webcface < Formula
   sha256 "71791c3710ab14c9efefd2af77f87a4ec64a57b1aaf6b38d452a4c8fbac78e1e"
   license "MIT"
 
+  depends_on "cmake" => :build
+  depends_on "git" => :build
+  depends_on "googletest" => :build
   depends_on "msgpack-cxx"
   depends_on "spdlog"
-  depends_on "cmake" => :build
-  depends_on "googletest" => :build
-  depends_on "git" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    require 'fileutils'
-    FileUtils.rmdir(%w(external/eventpp external/cinatra external/tclap))
+    rmdir(%w[external/eventpp external/cinatra external/tclap])
     system "git", "clone", "https://github.com/wqking/eventpp.git", "external/eventpp"
     system "git", "clone", "https://github.com/qicosmos/cinatra.git", "external/cinatra"
     system "git", "clone", "https://git.code.sf.net/p/tclap/code", "external/tclap"
@@ -25,15 +23,6 @@ class Webcface < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test webcface`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/webcface-server", "-h"
 
     (testpath/"CMakeLists.txt").write <<EOS
@@ -58,6 +47,5 @@ EOS
 EOS
     system "cmake", "-B", "build"
     system "cmake", "--build", "build"
-
   end
 end

76236da645842f2e34455768ccbd5d125bd3c91c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-10-03 16:12:40 +0000

    webcface-tools: add 1.0.0 bottle.

--- Formula/webcface-tools.rb
+++ Formula/webcface-tools.rb
@@ -5,6 +5,12 @@ class WebcfaceTools < Formula
   sha256 "84360c16df1a793a0425c48e0158dd6eac86892db4bb95447e9b749771e57ff6"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-1.0.0"
+    sha256 cellar: :any,                 ventura:      "6551d48ce8278a744fbaa5a1a1ab9616680842a8e814d3138c7f77c0cd7b6dfe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5d863e661a892420ec2bd43f1840227b335933a0cb4ce4d932ea1f9272b6550a"
+  end
+
   depends_on "cmake" => :build
   depends_on "webcface"
 

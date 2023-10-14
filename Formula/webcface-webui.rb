ecab497331bd544e1e005864c3e1629e27d36b23
na-trium-144 <na-trium-144@users.noreply.github.com>
2023-10-14 03:49:00 +0000

    Update packages
    Closes #10.
    
    Signed-off-by: na-trium-144 <100704180+na-trium-144@users.noreply.github.com>

--- Formula/webcface-webui.rb
+++ Formula/webcface-webui.rb
@@ -1,8 +1,8 @@
 class WebcfaceWebui < Formula
   desc "UI Application for WebCFace"
   homepage "https://github.com/na-trium-144/webcface-webui"
-  url "https://github.com/na-trium-144/webcface-webui/releases/download/v1.0.3/webcface-webui_1.0.3.tar.gz"
-  sha256 "716ee866ffdb870fb1c2c1240a51837f05f593c6acd4138778d5e841e769b946"
+  url "https://github.com/na-trium-144/webcface-webui/releases/download/v1.0.4/webcface-webui_1.0.4.tar.gz"
+  sha256 "81589b1eff5a8d6022ae444e8a83ad0f97ab889dd73757ef7dedb8b8d3dcf372"
   license "MIT"
 
   bottle do

2d6d1e4f92a48248acb979d4a7bda8c70146fa03
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-11-20 20:45:28 +0900

    a

--- Formula/webcface.rb
+++ Formula/webcface.rb
@@ -16,9 +16,9 @@ class Webcface < Formula
   depends_on "spdlog"
 
   def install
-    rmdir(%w[external/eventpp external/cinatra external/asio external/crow external/curl])
+    rmdir(%w[external/eventpp external/cli11 external/asio external/crow external/curl])
     system "git", "clone", "https://github.com/wqking/eventpp.git", "external/eventpp"
-    system "git", "clone", "https://github.com/qicosmos/cinatra.git", "external/cinatra"
+    system "git", "clone", "https://github.com/CLIUtils/CLI11.git", "external/cli11"
     system "git", "clone", "https://github.com/chriskohlhoff/asio.git", "external/asio"
     system "git", "clone", "https://github.com/curl/curl.git", "external/curl"
     system "git", "clone", "--branch", "fix-destructor-io-service", "https://github.com/na-trium-144/Crow.git",

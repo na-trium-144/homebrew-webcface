1041a68a6188c305d7e551a05d9f94a89bc2ec58
na-trium-144 <na-trium-144@users.noreply.github.com>
2024-01-17 11:49:37 +0000

    Update packages

--- Formula/webcface-webui.rb
+++ Formula/webcface-webui.rb
@@ -1,8 +1,8 @@
 class WebcfaceWebui < Formula
   desc "UI Application for WebCFace"
   homepage "https://github.com/na-trium-144/webcface-webui"
-  url "https://github.com/na-trium-144/webcface-webui/releases/download/v1.3.0/webcface-webui_1.3.0.tar.gz"
-  sha256 "711a8cc1be370129017c4714cab86e77c5be00902e84ff16198c94e9e6194de4"
+  url "https://github.com/na-trium-144/webcface-webui/releases/download/v1.3.1/webcface-webui_1.3.1.tar.gz"
+  sha256 "1a983cb9698e6cdaff218194eb30b7cfe269371600cccd50033f1be7cb17268f"
   license "MIT"
 
   bottle do

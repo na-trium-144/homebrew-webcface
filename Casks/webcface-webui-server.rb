24998c184cd183e35fc2f3340d56608f52e577c5
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-02-09 21:10:10 +0900

    sha修正

--- Casks/webcface-webui-server.rb
+++ Casks/webcface-webui-server.rb
@@ -1,6 +1,6 @@
 cask "webcface-webui-server" do
   version "1.5.3"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "d3c44dc41a39615a5e747827d589b2a7628bed8280c2bc360d5a6f84e0208b6c"
 
   url "https://github.com/na-trium-144/homebrew-webcface/releases/download/v#{version}/WebCFace.WebUI.Server.app.zip"
   name "WebCFace WebUI Server"

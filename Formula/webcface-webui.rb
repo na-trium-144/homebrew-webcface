b7be6940171b48fabb623892b5df9411f84522ef
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-02-15 03:50:25 +0000

    webcface-webui: update 1.4.0 bottle.

--- Formula/webcface-webui.rb
+++ Formula/webcface-webui.rb
@@ -6,9 +6,10 @@ class WebcfaceWebui < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-webui-1.3.2"
-    sha256 cellar: :any_skip_relocation, ventura:      "1cad25deb51953c3dbbccbc2f1d579601db03fea83ce3b589011d192156e6560"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d7d7db39ad8906cf4dff33c0e3487a3dda82f0d10f2694718a9d0b4505fc0f3e"
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-webui-1.4.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "b72ad365ce4804c5cdf4da1daacadc0fb269d92c72cc25d6565c87aa3fde0826"
+    sha256 cellar: :any_skip_relocation, ventura:      "3c63b4fec7a1b9784734ca1c058c900bf723b6eae575880199cd1bcca6070e68"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2da78a0cdbab6556397873b0d4dee5099903dbbe36595387d352e1dc1d94f06f"
   end
 
   def install

a7e72431a32894ab1bdb97c94a74f84bebe2a025
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-07-19 04:02:26 +0900

    update
    Closes #92.
    
    Signed-off-by: na-trium-144 <100704180+na-trium-144@users.noreply.github.com>

--- Formula/webcface-tools.rb
+++ Formula/webcface-tools.rb
@@ -4,6 +4,7 @@ class WebcfaceTools < Formula
   url "https://github.com/na-trium-144/webcface-tools/archive/refs/tags/v1.4.5.tar.gz"
   sha256 "a2647fb28c862cef7954fac58bc2954990f1f4b3ecd77dd2b64aa3548e49c9d2"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-1.4.5"

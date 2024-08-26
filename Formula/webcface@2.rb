7f2abe72467fbb2d95fe9c060e7dfe93cb5aaa92
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2024-08-26 04:42:18 +0000

    webcface@2: add 2.0.1 bottle.

--- Formula/webcface@2.rb
+++ Formula/webcface@2.rb
@@ -5,6 +5,14 @@ class WebcfaceAT2 < Formula
   sha256 "68f6c5b95027490c3ff9284410ab5e5a571870dff3dbeedb43aa605ffeb9edb8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface@2-2.0.1"
+    sha256 cellar: :any, arm64_sonoma: "b4e1443940a5320f7d9f638471e97729a9bac382a6c5b92ca46d1aa2f36eb113"
+    sha256 cellar: :any, ventura:      "c67fd221cf28d840fdedc427bc4ef4549b330fb2948f6f7a9461c40fb405663b"
+    sha256 cellar: :any, monterey:     "579c5f5e62c6f59c7b4833f0e6e48f7778dfc70fff7e01c33782f39aeae1f992"
+    sha256               x86_64_linux: "853523d22c12feac2a06220ef0b7e76db1b759cbbed2f89a9eabd0dfd921c33f"
+  end
+
   depends_on "asio" => :build
   depends_on "cli11" => :build
   depends_on "cmake" => [:build, :test]

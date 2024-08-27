10eff8831f7d0775ebe173e578e2044e451caae1
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2024-08-27 15:01:25 +0000

    webcface-tools: add 2.0.1 bottle.

--- Formula/webcface-tools.rb
+++ Formula/webcface-tools.rb
@@ -5,6 +5,14 @@ class WebcfaceTools < Formula
   sha256 "a43a82747ca60ea12a181845b620b63e8f13a330d157efb2cbb6be1a70f1a8fa"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-2.0.1"
+    sha256 cellar: :any, arm64_sonoma: "bceb3281b214e6080b3dd9515b41823e35a28c2ecead0b74eabc16988aa6a5fc"
+    sha256 cellar: :any, ventura:      "02851c5a72869a6eecd1eafa11dac625be11d1892f13d98a6150fd5c1596f021"
+    sha256 cellar: :any, monterey:     "4bb542fc608c97a9e947e654631a3bbebdd896db00dbbaa2184afb2578e0abdd"
+    sha256               x86_64_linux: "c51a90686471bfd6001a9fddfba15b40d4fd9c6864a5bb21beb9a36b75bba541"
+  end
+
   depends_on "cli11" => :build
   depends_on "cmake" => :build
   depends_on "ftxui-static" => :build

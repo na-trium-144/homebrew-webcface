16f6079946183b369d648eb99a5c8023398e52d8
na-trium-144 <na-trium-144@users.noreply.github.com>
2024-09-09 17:06:31 +0000

    Update packages
    Closes #101.
    
    Signed-off-by: k. Naka <100704180+na-trium-144@users.noreply.github.com>

--- Formula/webcface@2.rb
+++ Formula/webcface@2.rb
@@ -1,8 +1,8 @@
 class WebcfaceAT2 < Formula
   desc "Web-based IPC & Dashboard-like UI"
   homepage "https://na-trium-144.github.io/webcface/"
-  url "https://github.com/na-trium-144/webcface/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "d8a7a4e43d9f3abc68b68cfdefb3ec24e600aab6a7647ed4d68d9ed6a99453d4"
+  url "https://github.com/na-trium-144/webcface/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "28fd5f32b9b79c23d861d00219423cdfb7e18d639efd6c6d579e56240ecd7a75"
   license "MIT"
 
   bottle do

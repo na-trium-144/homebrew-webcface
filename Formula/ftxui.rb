62823e0befc08814221d1ef703f084c11a01ef61
na-trium-144 <na-trium-144@users.noreply.github.com>
2024-12-28 14:43:57 +0000

    Update packages

--- Formula/ftxui.rb
+++ Formula/ftxui.rb
@@ -2,7 +2,7 @@ class Ftxui < Formula
   desc ":computer: C++ Functional Terminal User Interface. :heart:"
   homepage "https://github.com/ArthurSonzogni/FTXUI"
   url "https://github.com/ArthurSonzogni/FTXUI.git",
-      revision: "c5357acbaa68efb2902db7b9257d3e1d291030d3"
+      revision: "6fafa2dfed9e1d5d4e56660206fec7fb8e1af7dd"
   version "5.0.0-66-gc5357ac"
   license "MIT"
 
@@ -10,9 +10,9 @@ class Ftxui < Formula
 
   def install
     File.open('CMakeLists.txt', 'a') do |file|
-      file.puts "set_target_properties(screen PROPERTIES VERSION 5.0.0.66)\n"
-      file.puts "set_target_properties(dom PROPERTIES VERSION 5.0.0.66)\n"
-      file.puts "set_target_properties(component PROPERTIES VERSION 5.0.0.66)\n"
+      file.puts "set_target_properties(screen PROPERTIES VERSION v5.0.0.82)\n"
+      file.puts "set_target_properties(dom PROPERTIES VERSION v5.0.0.82)\n"
+      file.puts "set_target_properties(component PROPERTIES VERSION v5.0.0.82)\n"
     end
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
     system "cmake", "--build", "build"
@@ -22,7 +22,7 @@ class Ftxui < Formula
   test do
     (testpath/"CMakeLists.txt").write <<~EOS
       cmake_minimum_required (VERSION 3.11)
-      project(ftxui-starter LANGUAGES CXX VERSION 1.0.0)
+      project(ftxui-starter LANGUAGES CXX VERSION v5.0.0.82)
       find_package(ftxui 5 REQUIRED)
       add_executable(ftxui-starter src/main.cpp)
       target_link_libraries(ftxui-starter

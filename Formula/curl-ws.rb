14991eb7dc65536ecd51d59b6e7fa9e6b9fc39be
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-12-31 13:45:14 +0900

    revert curl version to 8.9.1

--- Formula/curl-ws.rb
+++ Formula/curl-ws.rb
@@ -2,25 +2,17 @@ class CurlWs < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
   # Don't forget to update both instances of the version in the GitHub mirror URL.
-  # `url` goes below this comment when the `stable` block is removed.
-  url "https://curl.se/download/curl-8.11.1.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_11_1/curl-8.11.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-8.11.1.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-8.11.1.tar.bz2"
-  sha256 "e9773ad1dfa21aedbfe8e1ef24c9478fa780b1b3d4f763c98dd04629b5e43485"
+  url "https://curl.se/download/curl-8.9.1.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_9_1/curl-8.9.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-8.9.1.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-8.9.1.tar.bz2"
+  sha256 "b57285d9e18bf12a5f2309fc45244f6cf9cb14734e7454121099dd0a83d669a3"
   license "curl"
+  revision 1
 
   livecheck do
     url "https://curl.se/download/"
     regex(/href=.*?curl[._-]v?(.*?)\.t/i)
-  end
-
-  bottle do
-    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/curl-ws-8.11.1"
-    sha256 cellar: :any,                 arm64_sequoia: "0c77d6dfe2d05f2839c0757d6b305f6f534e55b1e4423f37d6e2e8927dcbfbe9"
-    sha256 cellar: :any,                 arm64_sonoma:  "930d540ebe595725c8d21d26349567847bb7c0edde58d3d738d057bd2ae56886"
-    sha256 cellar: :any,                 ventura:       "512958b9f567906d90b2729248c4381de1563377aabb2570963ee485eb150cfe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a547b2d7826075bd9f2306c89b3640c1893d80945905d03d7578238acaab5a33"
   end
 
   head do
@@ -33,8 +25,9 @@ class CurlWs < Formula
 
   keg_only "this conflict with curl"
 
-  depends_on "pkgconf" => [:build, :test]
+  depends_on "pkg-config" => :build
   depends_on "brotli"
+  depends_on "libidn2"
   depends_on "libnghttp2"
   depends_on "libssh2"
   depends_on "openssl@3"
@@ -44,10 +37,6 @@ class CurlWs < Formula
   uses_from_macos "krb5"
   uses_from_macos "openldap"
   uses_from_macos "zlib"
-
-  on_system :linux, macos: :monterey_or_older do
-    depends_on "libidn2"
-  end
 
   def install
     tag_name = "curl-#{version.to_s.tr(".", "_")}"
@@ -66,6 +55,7 @@ class CurlWs < Formula
       --with-ca-fallback
       --with-secure-transport
       --with-default-ssl-backend=openssl
+      --with-libidn2
       --with-librtmp
       --with-libssh2
       --without-libpsl
@@ -80,18 +70,6 @@ class CurlWs < Formula
       "--with-gssapi=#{Formula["krb5"].opt_prefix}"
     end
 
-    args += if OS.mac? && MacOS.version >= :ventura
-      %w[
-        --with-apple-idn
-        --without-libidn2
-      ]
-    else
-      %w[
-        --without-apple-idn
-        --with-libidn2
-      ]
-    end
-
     system "./configure", *args, *std_configure_args
     system "make", "install"
     system "make", "install", "-C", "scripts"
@@ -101,26 +79,12 @@ class CurlWs < Formula
   test do
     # Fetch the curl tarball and see that the checksum matches.
     # This requires a network connection, but so does Homebrew in general.
-    filename = testpath/"test.tar.gz"
+    filename = (testpath/"test.tar.gz")
     system bin/"curl", "-L", stable.url, "-o", filename
     filename.verify_checksum stable.checksum
 
-    # Check dependencies linked correctly
-    curl_features = shell_output("#{bin}/curl-config --features").split("\n")
-    %w[brotli GSS-API HTTP2 IDN libz SSL zstd].each do |feature|
-      assert_includes curl_features, feature
-    end
-    curl_protocols = shell_output("#{bin}/curl-config --protocols").split("\n")
-    %w[LDAPS RTMP SCP SFTP].each do |protocol|
-      assert_includes curl_protocols, protocol
-    end
-
     system libexec/"mk-ca-bundle.pl", "test.pem"
-    assert_path_exists testpath/"test.pem"
-    assert_path_exists testpath/"certdata.txt"
-
-    with_env(PKG_CONFIG_PATH: lib/"pkgconfig") do
-      system "pkgconf", "--cflags", "libcurl"
-    end
+    assert_predicate testpath/"test.pem", :exist?
+    assert_predicate testpath/"certdata.txt", :exist?
   end
 end

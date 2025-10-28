f82e321a27f0243b235b6e0b5bc36ab5e130d7c2
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2025-10-28 07:09:19 +0000

    webcface-tools: add 3.0.1 bottle.

--- Formula/webcface-tools.rb
+++ Formula/webcface-tools.rb
@@ -5,6 +5,13 @@ class WebcfaceTools < Formula
   sha256 "d8ed95c69b3a914933492547dce7d9b55ebd7d094b959d47b42e0ab2878d02aa"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-3.0.1"
+    sha256 cellar: :any, arm64_sequoia: "82a48f2ae3e9bac5016ffc28d418773f93a1575d53eebceaaa85d2f1a7c8e889"
+    sha256 cellar: :any, arm64_sonoma:  "43d91129e7f09e0742dfdabb7f3d77a3751d6552491020c607010a8daa34ecaf"
+    sha256               x86_64_linux:  "abe3a12149b6fd1d47b1d213f165cfda39d50e45f65600f7e41f316a7e336497"
+  end
+
   depends_on "cli11" => :build
   depends_on "cmake" => :build
   depends_on "meson" => :build

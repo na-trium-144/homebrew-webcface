0b5719abb5f622594f8cad22ecdf6239106138d1
k. Naka <100704180+na-trium-144@users.noreply.github.com>
2026-01-07 01:24:38 +0000

    webcface-tools: update 3.0.1_2 bottle.

--- Formula/webcface-tools.rb
+++ Formula/webcface-tools.rb
@@ -7,10 +7,10 @@ class WebcfaceTools < Formula
   revision 2
 
   bottle do
-    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-3.0.1_1"
-    sha256 cellar: :any, arm64_sequoia: "39376e1977d876b05258c0e43f4207d7e80032ef4b6eb55f088e51eb8b74cc37"
-    sha256 cellar: :any, arm64_sonoma:  "f280bda267395ca30f5742496cb24c1d887d1f4dca4da7c7685f56cff03b6c50"
-    sha256               x86_64_linux:  "a46dadb628e7545ef9e860b637fc7ba363e37a6399e0906fd52045274570b896"
+    root_url "https://github.com/na-trium-144/homebrew-webcface/releases/download/webcface-tools-3.0.1_2"
+    sha256 cellar: :any, arm64_sequoia: "eef4d26adc88008a87e869e152c2ab61f4b790d23f8978a376c173bcae8ff488"
+    sha256 cellar: :any, arm64_sonoma:  "23a7c33c0aa2ada9c0a599c49b721bfc9ee29dac614458b4b2ec135d87306c90"
+    sha256               x86_64_linux:  "24f05c5a9d0a190953cdf35eb8ba9e62353b044c0c661bb7e804075cd90675a0"
   end
 
   depends_on "cli11" => :build
